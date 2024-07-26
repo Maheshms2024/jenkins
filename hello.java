a4487a7cbd63735fd1a386e843319cf39c98ac5c
Maheshms2024 <maheshshamanthgowda@gmail.com>
2024-07-26 12:42:34 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    // The main method is the entry point of any Java application.
-    public static void main(String[] args) {
-        // Print "Hello, World!" to the console.
-        System.out.println("Hello, World!");
-    }
-}
